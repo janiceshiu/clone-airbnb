1a7bb390733eb763c7e2a8e4952b38574eef2991
contrepoint <contrepoint21@gmail.com>
2016-03-02 17:10:47 +0800

    basic transactions work. but braintree amount is fixed. must use a variable

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -3,25 +3,24 @@ class PaymentsController < ApplicationController
 
 	def new
 		@reservation = Reservation.find(params[:reservation_id])
-		@no_of_nights = (@reservation.check_out_date - @reservation.check_in_date).to_i # sample numbers
-		@rent_per_night = @reservation.listing.rent_per_night # sample numbers
+		@no_of_nights = (@reservation.check_out_date - @reservation.check_in_date).to_i
+		@rent_per_night = @reservation.listing.rent_per_night
 		@total_payment = total_payment(@rent_per_night, @no_of_nights)
-		# @client_token = gon.client_token = generate_client_token
+		@client_token = gon.client_token = generate_client_token
 		# @client_token = generate_client_token
 	end
 
 	def create
-		byebug
     @result = Braintree::Transaction.sale(
               amount: 1000,		 #  test number
               payment_method_nonce: params[:payment_method_nonce])
     byebug
     if @result.success?
-      redirect_to root_url, notice: "Congraulations! Your transaction has been successful!"
+      redirect_to root_path, notice: "Congratulations! Your transaction is successful. You paid #{@total_payment}."
     else
       flash[:alert] = "Something went wrong while processing your transaction. Please try again!"
       gon.client_token = generate_client_token
-      render :new
+      redirect_to help_path
     end
   end
 
